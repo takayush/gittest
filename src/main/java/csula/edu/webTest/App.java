5355cd5b636e01f1608ce9943204dbb57d4fd491
takayush <ayush.tak@gmail.com>
2016-09-12 18:39:53 -0700

    Initial Commit

---
+++ src/main/java/csula/edu/webTest/App.java
@@ -0,0 +1,13 @@
+package csula.edu.webTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
